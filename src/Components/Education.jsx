390487f2b058b62eab946193781d300f04fa5927
Goldjunge91 <m.tozzi91@icloud.com>
2024-01-05 00:08:16 +0000

    Update About and Home components, add Education component, and modify Portfolio component

---
+++ src/Components/Education.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function Education() {
+  return (
+    <section id="education" className="min-height">
+      <div className="content">
+        <h1>Education</h1>
+        <p>
+          <strong>Technische Universität Dresden</strong>
+          <br />
+          <em>2012 - 2019</em>
+          <br />
+          <em>Master of Science in Mechanical Engineering</em>
+        </p>
+        <p>
+          <strong>Technische Universität Dresden</strong>
+          <br />
+          <em>2010 - 2012</em>
+          <br />
+          <em>Bachelor of Science in Mechanical Engineering</em>
+        </p>
+        <p>
+          <strong>Technische Universität Dresden</strong>
+          <br />
+          <em>2009 - 2010</em>
+          <br />
+          <em>Preparatory course for international students</em>
+        </p>
+        <p>
+          <strong>Technische Universität Dresden</strong>
+          <br />
+          <em>2008 - 2009</em>
+          <br />
+          <em>German language course</em>
+        </p>
+        <p>
+          <strong>Universidad de Cádiz</strong>
+          <br />
+          <em>2007 - 2008</em>
+          <br />
+          <em>Erasmus semester</em>
+        </p>
+        <p>
+          <strong>Technische Universität Dresden</strong>
+          <br />
+          <em>2006 - 2007</em>
+          <br />
+          <em>Preparatory course for international students</em>
+        </p>
+        <p>
+          <strong>Deutsche Schule Madrid</strong>
+          <br />
+          <em>1994 - 2006</em>
+          <br />
+          <em>German Abitur</em>
+        </p>
+      </div>
+    </section>
+  );
+}
